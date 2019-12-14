ee47cb598a1a858ed57ec52a75dabe038e917137
Mohammed Jawad <al7joker111@gmail.com>
2019-12-14 14:54:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 
 // Write your code here
+<<<<<<< HEAD
 let num1=1;
 let num2=62;
 let multiply=num1*num2;
 const random=Math.floor(Math.random()*1000)+1
 const mod=16%6
 const max=Math.max(1,2,3,20) 
+=======
+
+num1 = 31
+num2 =  2
+let multiply = num1 * num2 ;
+let random = Math.random() * 10 ;
+let mod = num1 % 9 
+let max = Math.max(12 ,20 ,5)
+>>>>>>> 770c9b3bab385c23c941063d434b886e335d7789
